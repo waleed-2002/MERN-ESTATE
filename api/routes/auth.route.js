f93f6a0dcc730d0cfa7eabacdd757db6e002dc53
waleed <waleed.ashraf1601@gmail.com>
2024-09-27 16:41:12 +0500

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 const router = express.Router(); // Correct initialization of the router
 
 router.post("/signup", signup); // This line is fine
+router.post("/signin", signin); // This line is fine
 
 export default router;
