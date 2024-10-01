01e4ee46c57c0548c11cf76d00f2f876bf95614d
waleed <waleed.ashraf1601@gmail.com>
2024-10-01 09:04:37 +0500

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,15 +5,11 @@ import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    const validPassword = bcrypt.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
-
     await newUser.save();
-    res.status(201).json("User created successfully");
+    res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
   }
